3866b737544c0b124cc79c759510f484c40d737f
Roozbeh <roozbehsayadi@gmail.com>
2020-07-13 14:39:55 +0430

    Got the user role from back-end and saved it in state.

--- back/src/app.js
+++ back/src/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const router = require('./api/api')
 const port = 9000
 const db = require('./database/db.js')
-var {logger} = require ('./logger')
+var { logger } = require('./logger')
 require('dotenv').config()
 
-app.use ((req , res , next) => {
-    logger.info('info' , 'error')
-    next()
+app.use(cors())
+
+app.use((req, res, next) => {
+	logger.info('info', 'error')
+	next()
 })
 app.use('/', router)
 app.listen(port, () =>
-    console.log(`App listening at http://localhost:${process.env.port}`)
+	console.log(`App listening at http://localhost:${process.env.port}`)
 )
-db.connect();
+db.connect()
