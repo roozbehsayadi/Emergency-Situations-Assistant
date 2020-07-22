c8585bce74eb927a0acf18b7b4ca83eb023df52a
Roozbeh <roozbehsayadi@gmail.com>
2020-07-22 13:25:08 +0430

    Fixed some errors in backend.

--- back/src/api/api.js
+++ back/src/api/api.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const router = express.Router();
-const transferData = require('../logic/answers.js');
-const users = require('../logic/users.js');
-const userController = require('../controller/usersController');
+const express = require('express')
+const router = express.Router()
+const transferData = require('../logic/answers.js')
+const users = require('../logic/users.js')
+const userController = require('../controller/usersController')
 const formController = require('../controller/formsController.js')
 const answerController = require('../controller/answersController')
-const { json } = require('express');
+const { json } = require('express')
 var logger = require('../logger')
 require('dotenv').config()
 const jwt = require('express-jwt')
 const jwtAuthz = require('express-jwt-authz')
-const jwksRsa = require('jwks-rsa');
-const { SigningKeyNotFoundError } = require('jwks-rsa');
+const jwksRsa = require('jwks-rsa')
+const { SigningKeyNotFoundError } = require('jwks-rsa')
 
 const checkJwt = jwt({
-
 	secret: jwksRsa.expressJwtSecret({
 		cache: true,
 		rateLimit: true,
@@ -25,51 +24,30 @@ const checkJwt = jwt({
 	audience: process.env.api_identifier,
 	issuer: `https://${process.env.domain}/`,
 	algorithms: ['RS256'],
-
 })
 
 router.get('/forms', checkJwt, formController.getAll)
-///////////////////////////////////////////////unit test
-describe('GET /films-list', () => {
-    it('should return a list of films when called', done => {
-      chai
-        .request(app)
-        .get('/films-list')
-        .end((err, res) => {
-          res.should.have.status(200);
-          expect(res.body).to.deep.equal(starwarsFilmListMock);
-          done();
-        });
-    });
-  });
-//////////////////////////////////////////////unit test
 
 router.get('/role', checkJwt, userController.getRole)
 
-router.post('/forms/:id(\\d+)', checkjwt ,express.json(), answerController.add)
+router.post('/forms/:id(\\d+)', checkJwt, express.json(), answerController.add)
 
-router.get('/forms/:id(\\d+)' ,checkJwt, (req, res) => {
+router.get('/forms/:id(\\d+)', checkJwt, (req, res) => {
+	username = req.user['https://example.com/email']
 
-    username = req.user['https://example.com/email']
-
-    users
-        .getUserRole(username)
-        .then ((role) => {
-            if (role == "control_center") {
-                answerController.getOne(req , res);
-            }
-            else if (role == "field_agent"){
-                formController.getOne(req , res) ;
-            }
-            else {
-                logger.log(
-                    `access denied for get request ${req.originalUrl} for user ${username}`
-                )
-                res.status(403).send({
-                    message : "access denied"
-                })
-            }
-    })
-
+	users.getUserRole(username).then((role) => {
+		if (role == 'control_center') {
+			answerController.getOne(req, res)
+		} else if (role == 'field_agent') {
+			formController.getOne(req, res)
+		} else {
+			logger.log(
+				`access denied for get request ${req.originalUrl} for user ${username}`
+			)
+			res.status(403).send({
+				message: 'access denied',
+			})
+		}
+	})
 })
 module.exports = router
