605495fcebad777f35400045e35b981bf870bdef
amirali <amirali.mirian@gmail.com>
2020-07-23 00:48:36 +0430

    added search bars. but after filtering, we have wrong index in the modal(FeelsBadMan).

--- front/src/components/ControlCenterAgentSubmissionList.js
+++ front/src/components/ControlCenterAgentSubmissionList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import {Table, Modal} from "antd";
+import {Table, Modal, Input, Button, Space} from "antd";
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 import {Layout} from "antd";
 import {withRouter} from 'react-router-dom';
 import {Map, Marker, GoogleApiWrapper} from "google-maps-react";
@@ -16,6 +18,8 @@ class ControlCenterAgentSubmissionList extends React.Component {
             visible: false,
             modal_title: '',
             modal_content: [],
+            searchText: '',
+            searchedColumns: '',
         }
         this.handleTableCreation = this.handleTableCreation.bind(this)
     }
@@ -49,6 +53,69 @@ class ControlCenterAgentSubmissionList extends React.Component {
         this.setState({
             visible: false,
         });
+    };
+
+    getColumnSearchProps = dataIndex => ({
+        filterDropdown: ({setSelectedKeys, selectedKeys, confirm, clearFilters}) => (
+            <div style={{padding: 8}}>
+                <Input
+                    ref={node => {
+                        this.searchInput = node;
+                    }}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{width: 188, marginBottom: 8, display: 'block'}}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined/>}
+                        size="small"
+                        style={{width: 90}}
+                    >
+                        Search
+                    </Button>
+                    <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{width: 90}}>
+                        Reset
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: filtered => <SearchOutlined style={{color: filtered ? '#1890ff' : undefined}}/>,
+        onFilter: (value, record) =>
+            record[dataIndex] ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()) : '',
+        onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+                setTimeout(() => this.searchInput.select());
+            }
+        },
+        render: text =>
+            this.state.searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{backgroundColor: '#ffc069', padding: 0}}
+                    searchWords={[this.state.searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
+
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        this.setState({
+            searchText: selectedKeys[0],
+            searchedColumn: dataIndex,
+        });
+    };
+
+    handleReset = clearFilters => {
+        clearFilters();
+        this.setState({searchText: ''});
     };
 
     createMapContents(locations) {
@@ -101,7 +168,7 @@ class ControlCenterAgentSubmissionList extends React.Component {
         let columns = []
         columns.push(...[
             {title: '#', dataIndex: 'key', key: 'key', width: '1%',},
-            {title: 'username', dataIndex: 'username', key: 'username',},
+            {title: 'username', dataIndex: 'username', key: 'username', ...this.getColumnSearchProps('username')},
         ])
 
         if (this.state.answers.length > 0) {
@@ -110,6 +177,7 @@ class ControlCenterAgentSubmissionList extends React.Component {
                     title: value.title,
                     dataIndex: value.name,
                     key: value.name,
+                    ...this.getColumnSearchProps(value.name),
                 }
                 columns.push(column)
             })
