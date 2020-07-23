73b4dab9ffe17682c87f6bc290e579e80c33749a
azinazarkar <azin.azarkar@gmail.com>
2020-07-23 18:47:21 +0430

    login completed

--- back/src/logic/users.js
+++ back/src/logic/users.js
@@ -5,13 +5,25 @@ const { json } = require('express');
 
 
 exports.getUserRole = function(userName){
+
     return new Promise ((res , rej) => {
+
         let jsonStr = " {\"username\" : \"" + userName + "\" }" ;
         let role ;
         db.find("users" , jsonStr)
             .then ((val) => {
                 if (val == null) {
-                    res(null)
+
+                    let newRed = {}
+                    newRed["username"] = userName ;
+                    newRed["role"] = "control-center"
+                    db.insert("users" , newRed).then ((added) => {
+                        res("control-center")
+                    })
+                    .catch((err) => {
+                        rej(err)
+                    })
+
                 }
                 else {
                     res(val.role);
